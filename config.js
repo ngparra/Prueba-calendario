c7fd6232c978ceffbc8856526ea59a522de9f088
Nicole Parra <62161098+ngparra@users.noreply.github.com>
2026-09-09 17:00:49 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://wjzlhvszimofdppognek.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_uxG-rRn7Iyxc5RrbqW7TYg_Z5BDhSZu",
+  TIMEZONE: "America/Costa_Rica",
+  ALLOWED_EMAIL_DOMAIN: "ucr.ac.cr"
+};
